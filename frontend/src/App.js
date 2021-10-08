6f1fe739c397e0f353b4fefb3ac164062ed2fb0c
shirishtijain <universej2002@gmail.com>
2021-10-09 00:49:11 +0400

    added login , register page and corrected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login';
+import Register from './components/Register';
+
+
 
 function App() {
   return (
-    <div className="app">
-    
-    </div>
+  
+    <Router>
+<div className="app">
+  
+  <Switch>
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+   
+  </Switch>
+</div>
+</Router>
+  
   );
 }
 
