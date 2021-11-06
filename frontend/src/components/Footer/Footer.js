37e7e40f07fa3b42068a89edf3db331a379de597
Arushee Verma <aru2002verma@gmail.com>
2021-11-07 00:37:47 +0530

    footer upload

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import "./Footer.css";
+function Footer() {
+    return (
+        <div>
+            
+      
+      
+<div className="page-footer font-small special-color-dark pt-4 " id="footer">
+    
+
+    
+    <div className="container">
+  
+     
+      <div className="row">
+
+    
+    
+    <div className="col">
+        <iframe className="well well-sm  " id="embeddedMap" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14010.633789636884!2d77.0379647!3d28.6100216!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x108adaa3abe4bd07!2sNetaji%20Subhas%20University%20of%20Technology!5e0!3m2!1sen!2sin!4v1636115674060!5m2!1sen!2sin" width="400" height="250" style={{border:"0"}} allowfullscreen="" loading="lazy"></iframe>
+    </div>
+    <div className="col">
+        <h5 className="font-weight-bold text-white mb-4">  CONTACT US</h5>
+
+        <p id="contactUs"> <span id="email"><i  className="fa">&#xf0e0;</i>  Email:</span><a className="mail" href="mailto:covidya.sam@nsut.ac.in.com">Covidya.sam@nsut.ac.in</a></p>
+        <p id="contactUs"> <span id="address"><i  className="fa">&#xf041;</i>  Address:</span> Netaji Subhas University of Technology, Azad Hind Fauj Marg,Sector - 3 , Dwarka, New Delhi, India</p>
+        <p id="contactUs"><span id="phone"><i className="fas fa-phone-square"></i></span> Call: 011-2876XXXX</p>
+    
+    </div>
+    </div>
+
+    </div>
+
+    <div classNameName="row">
+
+        
+        <div classNameName="col-md-12 py-1">
+            
+          <div classNameName="mb-5 flex-center">
+
+            
+
+
+            
+            
+            <a classNameName="fb-ic">
+              <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            
+            <a classNameName="tw-ic">
+              <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            
+            
+            <a classNameName="li-ic">
+              <i classNameName="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            
+            <a classNameName="ins-ic">
+              <i classNameName="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            
+            <a classNameName="pin-ic">
+              <i classNameName="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+            </a>
+          </div>
+        </div>
+       
+  
+      </div>
+      
+  
+    </div>
+    
+  
+    
+    <div classNameName="footer-copyright text-center py-3">© 2021 Copyright:
+      <a href="https://mdbootstrap.com/"> CoVidya.com</a>
+    </div>
+    
+  
+  </div>
+  
+
+        
+    )
+}
+
+export default Footer
