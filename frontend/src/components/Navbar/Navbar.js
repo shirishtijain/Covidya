b68d577df61c144a6a169e2997bc66bc7cb1c672
Manan Suri <manansuri27@gmail.com>
2021-12-07 16:12:30 +0530

    empty chang

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,15 +1,6 @@
 import React from 'react'
-import { Navbar, Container, Nav ,Button} from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux';
-import { logout, clearErrors } from "../../actions/userActions";
-
+import {Navbar, Container, Nav} from 'react-bootstrap'
 function NavbarMain() {
-    const { isAuthenticated } = useSelector(state => state.auth);
-    const dispatch = useDispatch();
-    const handleLogout = (e) => {
-     
-        dispatch(logout());
-      }
     return (
 
         <Navbar bg="primary" variant="dark">
@@ -21,19 +12,8 @@ function NavbarMain() {
           <Nav.Link href="/map">Map</Nav.Link>
           <Nav.Link href="/resources">Resources</Nav.Link>
           <Nav.Link href="/guidelines">Guidelines</Nav.Link>
-                    {isAuthenticated ? 
-                        <>
-                        <Nav.Link href="/login">Login</Nav.Link>  
+          <Nav.Link href="/login">Login</Nav.Link>  
           <Nav.Link href="/register">Register</Nav.Link>
-          </>
-                        
-                     : (
-                            <>
-                            <Button href="/logout" onClick={handleLogout}>Logout</Button>  
-                     </>       
-                            
-        )}
-
 
         </Nav>
         </Container>
